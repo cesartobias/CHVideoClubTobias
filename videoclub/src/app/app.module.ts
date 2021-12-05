5dc496f0b04e7fa8783284cbf94178e1bcd4c02f
Cesar Tobias <cesar.tobias@gmail.com>
2021-12-05 14:06:56 -0600

    se crean componentes: registro, login, listado, info, carrito

--- videoclub/src/app/app.module.ts
+++ videoclub/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegistroComponent } from './components/registro/registro.component';
+import { RegistrComponent } from './components/registr/registr.component';
+import { LoginComponent } from './components/login/login.component';
+import { ListadoComponent } from './components/listado/listado.component';
+import { InfoComponent } from './components/info/info.component';
+import { CarritoComponent } from './components/carrito/carrito.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistroComponent,
+    RegistrComponent,
+    LoginComponent,
+    ListadoComponent,
+    InfoComponent,
+    CarritoComponent
   ],
   imports: [
     BrowserModule,
